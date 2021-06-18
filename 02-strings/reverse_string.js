41be42cee8f1f38511fa70612d009168fda0a852
ipobrapturemedia <ipobrapturemedia@gmail.com.com>
2021-06-18 15:54:56 +0200

    excercise 2 done

--- 02-strings/reverse_string.js
+++ 02-strings/reverse_string.js
@@ -1,1 +1,10 @@
 // read carefully the instructions
+const myString = 'hello we are at school'
+let stringArray = myString.split("");
+console.log(stringArray);
+const reversed = stringArray.reverse();
+console.log('reversed:', stringArray);
+console.log(stringArray.join(''))
+// let myStringReversed = stringArray.join('');
+// console.log('my string:\t\t', myString);
+// console.log('my string reversed:\t', myStringReversed);
