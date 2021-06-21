b6a8bd6aa05c567a4ff530b8bdda4e20832f0f1e
ipobrapturemedia <ipobrapturemedia@gmail.com.com>
2021-06-21 12:24:49 +0200

    assignemt 3 done

--- 03-multiples-of-number/multiples_of_number.js
+++ 03-multiples-of-number/multiples_of_number.js
@@ -1,1 +1,10 @@
 // define your function here
+const multiplesOf = (number, limit) => {};
+let MultipleOf = '';
+
+for (let i = 0; i < 12; i++) {
+  MultipleOf = MultipleOf + i;
+}
+console.log(MultipleOf);
+console.log (multiplesOf);
+const multiplesOfNumber = multiplesOf(01234567891011);
