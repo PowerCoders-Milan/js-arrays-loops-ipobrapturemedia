721305dc0fb36cb4dc325d8f93e1d57413e99476
Jackson Moses <ipobrapturemedia@gmail.com>
2021-06-21 20:02:41 +0200

    assignment 1 modified

--- 01-array/array.js
+++ 01-array/array.js
@@ -1,7 +1,6 @@
 // define an array with 5 elements
 const cities = ['frankfurt','madrid', 'beiging', 'oslo', 'paris'];
 // console.log() the 3rd element
-cities [3] = 'beiging';
-console.log ('beiging');
-
+cities [3] = 'oslo';
+console.log ('oslo');
 
